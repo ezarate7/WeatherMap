a6150d4614d0ff7f177d1a823b42ff9958004b33
Eddie Z <e.zarate7@hotmail.com>
2022-09-12 11:40:27 -0600

    Add project files.

---
+++ WeatherMap/Program.cs
@@ -0,0 +1,7 @@
+﻿
+var client = new HttpClient();
+var apiKey = "1f715b864db491a2252a06c9aa078b57";
+
+
+Console.WriteLine("What city are do you want the weather for?");
+var cityName = Console.ReadLine();
